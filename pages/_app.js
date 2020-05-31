a30732f199520a12c530ea257d006ea954a48e10
Taylor <trm313@gmail.com>
2020-05-31 02:07:05 -0400

    Added google analytics tag and functions

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,20 @@
+import { useEffect } from "react";
+import Router from "next/router";
+import * as gtag from "../lib/gtag";
+
 import "../styles/fonts.css";
 import "../styles/index.css";
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    Router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      Router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, []);
+
   return <Component {...pageProps} />;
 }
--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,8 @@
 import Document, { Head, Main, NextScript } from "next/document";
 import React from "react";
 
+import { GA_TRACKING_ID } from "../lib/gtag";
+
 class CustomDocument extends Document {
   render() {
     return (
@@ -14,6 +16,23 @@ class CustomDocument extends Document {
             href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css"
             rel="stylesheet"
           ></link>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
         </Head>
         <body>
           <Main />
