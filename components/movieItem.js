4d2bb34b3bad23c7c0e97ab9503254e033dfa740
Taylor <trm313@gmail.com>
2020-07-19 14:43:23 -0400

    upped the NEW flag to 14 days

--- components/movieItem.js
+++ components/movieItem.js
@@ -7,26 +7,26 @@ import Ratings from "../components/ratings";
 const MovieItem = ({ movie: { id, Movie, Year, omdb, slug, Created } }) => {
   return (
     <li className={`my-4 flex`} style={{ height: "125px" }}>
-      <div className="" style={{ minWidth: "85px" }}>
+      <div className='' style={{ minWidth: "85px" }}>
         <LazyLoad height={125}>
           <img src={omdb.Poster} style={{ height: "125px" }} />
         </LazyLoad>
       </div>
 
-      <div className="ml-4 flex flex-col justify-around py-1">
-        <Link href="/movies/[...id]" as={`/movies/${slug.join("/")}`}>
-          <a className="">
+      <div className='ml-4 flex flex-col justify-around py-1'>
+        <Link href='/movies/[...id]' as={`/movies/${slug.join("/")}`}>
+          <a className=''>
             <span>
               {Movie} ({Year})
             </span>
-            {differenceInDays(new Date(), new Date(Created)) <= 7 && (
-              <span className="uppercase text-2xs ml-2 py-1 px-2 rounded bg-green-400 text-white">
+            {differenceInDays(new Date(), new Date(Created)) <= 14 && (
+              <span className='uppercase text-2xs ml-2 py-1 px-2 rounded bg-green-400 text-white'>
                 New
               </span>
             )}
           </a>
         </Link>
-        <Ratings size="sm" ratings={omdb.Ratings} id={id} />
+        <Ratings size='sm' ratings={omdb.Ratings} id={id} />
       </div>
     </li>
   );
